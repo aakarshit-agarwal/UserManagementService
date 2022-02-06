1a7b12980eac440e8d0ca1dfbce096c435eb4787
Aakarshit Agarwal <aakagarwal@microsoft.com>
2022-02-07 02:27:41 +0530

    Adding Zipkin support

--- src/main/java/com/fitness/UserManagementService/UserManagementServiceApplication.java
+++ src/main/java/com/fitness/UserManagementService/UserManagementServiceApplication.java
@@ -1,12 +1,18 @@
 package com.fitness.UserManagementService;
 
+import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
 public class UserManagementServiceApplication {
+	@Bean
+	public Sampler alwaysSampler() {
+		return Sampler.ALWAYS_SAMPLE;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserManagementServiceApplication.class, args);
